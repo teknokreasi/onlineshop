4e4cf02443146cb16826f29b9fc419545014e17f
Tekno Kreasi <teknokreasiapps2@gmail.com>
2025-06-24 18:09:20 +0700

    a

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -31,11 +31,11 @@ const HeaderTop = () => {
         <ul className="flex items-center h-full gap-x-5 max-[370px]:text-sm max-[370px]:gap-x-2">
           <li className="flex items-center gap-x-2 font-semibold">
             <FaHeadphones className="text-white" />
-            <span>+62 896 0106 9294</span>
+            <span>+6289601069294</span>
           </li>
           <li className="flex items-center gap-x-2 font-semibold">
             <FaRegEnvelope className="text-white text-xl" />
-            <span>globaltujuhcompany@gmail.com</span>
+            <span>globaltujuh@gmail.com</span>
           </li>
         </ul>
         <ul className="flex items-center gap-x-5 h-full max-[370px]:text-sm max-[370px]:gap-x-2 font-semibold">
