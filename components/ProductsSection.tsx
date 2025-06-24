766c50d961272a84c5e0f185c56445f453f08fee
Tekno Kreasi <teknokreasiapps2@gmail.com>
2025-06-24 16:08:15 +0700

    a

--- components/ProductsSection.tsx
+++ components/ProductsSection.tsx
@@ -15,11 +15,11 @@ import Heading from "./Heading";
 const ProductsSection = async () => {
   // sending API request for getting all products
   const data = await fetch("http://localhost:3001/api/products");
-  return (
-    <div>{JSON.stringify(data)}</div>
-  );
-  console.log(data);
   const products = await data.json();
+  console.log(products);
+  return (
+    <div>{JSON.stringify(products)}</div>
+  );
   return (
     <div className="bg-blue-500 border-t-4 border-white">
       <div className="max-w-screen-2xl mx-auto pt-20">
