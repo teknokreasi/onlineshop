bca762bb3f242222211b807da83547fa6633106e
Tekno Kreasi <teknokreasiprojects@gmail.com>
2025-06-24 05:39:52 +0000

    first commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import { Account, User as AuthUser } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
+import prisma from "@/utils/db";
+import { nanoid } from "nanoid";
+import { authOptions } from "@/lib/authOptions";
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
